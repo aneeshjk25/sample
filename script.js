59e67a0a30f0703054ab68100000be41a380784c
aneeshjk13 <kallarakkal@hotmail.com>
2016-03-12 19:49:25 +0530

    Added bower install deploy actions hook for openshift added host for running in openshift or your changes. Lines starting

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+	port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/app'));
 
-app.listen(port);
+app.listen(port,ipaddress);
